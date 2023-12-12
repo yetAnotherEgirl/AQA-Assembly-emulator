b0976a40dc5d9189bf488d69a4352178255652b8
yetAnotherEgirl <emily.mosforth@proton.me>
2023-12-12 12:11:42 +0000

    init

---
+++ AqaAssemEmulator-GUI/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AqaAssemEmulator_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Tabs = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            Settings = new TabPage();
+            Tabs.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Tabs
+            // 
+            Tabs.Controls.Add(tabPage1);
+            Tabs.Controls.Add(tabPage2);
+            Tabs.Controls.Add(tabPage3);
+            Tabs.Controls.Add(Settings);
+            Tabs.Location = new Point(12, 12);
+            Tabs.Name = "Tabs";
+            Tabs.SelectedIndex = 0;
+            Tabs.Size = new Size(1150, 1070);
+            Tabs.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(8, 46);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1134, 1016);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(8, 46);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1134, 1016);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Location = new Point(8, 46);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(1134, 1016);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "tabPage3";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Settings
+            // 
+            Settings.Cursor = Cursors.Cross;
+            Settings.Location = new Point(8, 46);
+            Settings.Name = "Settings";
+            Settings.Padding = new Padding(3);
+            Settings.Size = new Size(1134, 1016);
+            Settings.TabIndex = 3;
+            Settings.Text = "Settings";
+            Settings.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1174, 1094);
+            Controls.Add(Tabs);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            Tabs.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl Tabs;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage Settings;
+    }
+}
---
+++ AqaAssemEmulator-GUI/Form1.cs
@@ -0,0 +1,19 @@
+namespace AqaAssemEmulator_GUI
+{
+    public partial class Form1 : Form
+    {
+
+     // readonly Size size = new Size(800, 600) * 2;
+
+        public Form1()
+        {
+          InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+    }
+}
